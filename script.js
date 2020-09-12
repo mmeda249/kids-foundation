decab6238d988c223955a84dcb9c08daec157f46
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-12 13:59:54 -0400

    updated contact form, added JavaScript interactivity, adjusted form colors

--- script.js
+++ script.js
@@ -23,12 +23,50 @@ $('.navbar-nav>li>a').on('click', function(){
   $('.navbar-collapse').collapse('hide');
 });
 
+// AJAX for contact form submissions
 
-// Alert after user has submitted a message from contact form - this code may be removed after further revisions
-var form = document.getElementById('contact_form');
+window.addEventListener("DOMContentLoaded", function() {
 
-function myFunction() {
-  if (form.checkValidity()) {
-    alert("Message sent! Thanks for filling out the form!");
+  // get the form elements defined in your form HTML above
+  
+  var form = document.getElementById("my-form");
+  var button = document.getElementById("my-form-button");
+var status = document.getElementById("my-form-status");
+
+  // Success and Error functions for after the form is submitted
+  
+  function success() {
+    form.reset();
+    button.style = "display: none ";
+    status.innerHTML = " &#10003 Thanks! We'll be in touch soon!";
   }
+
+  function error() {
+    status.innerHTML = " &#10005 Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+//helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
 }
